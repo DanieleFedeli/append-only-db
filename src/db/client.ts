25ad8e360b5f2ec85cb25ff15b735b21e821a766
danielefedeli <daniele.fedeli@infinitaslearning.com>
2025-03-09 22:40:24 +0100

    Add Biome formatter and update code style
    Format TypeScript files with the new Biome configuration

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,115 +1,124 @@
-import fs from 'node:fs'
-import type { KeyvStoreAdapter, StoredData, } from 'keyv'
-import { EventEmitter } from 'node:events'
+import { EventEmitter } from "node:events";
+import fs from "node:fs";
+import type { KeyvStoreAdapter, StoredData } from "keyv";
 
 export interface AppendOnlyKeyvOpts {
-  namespace?: string
+	namespace?: string;
 }
 
 type Message = {
-  payload: string | undefined;
-  timestamp: number;
-  deleted?: boolean;
-}
+	payload: string | undefined;
+	timestamp: number;
+	deleted?: boolean;
+};
 
 export class AppendOnlyKeyv extends EventEmitter implements KeyvStoreAdapter {
-  private fd: number
-  private offset = 0
-  private index: Record<string, { offset: number, length: number } | undefined> = {}
-  private filePath: string
-  namespace?: string | undefined
+	private fd: number;
+	private offset = 0;
+	private index: Record<
+		string,
+		{ offset: number; length: number } | undefined
+	> = {};
+	private filePath: string;
+	namespace?: string | undefined;
 
-  constructor(filePath: string, opts: AppendOnlyKeyvOpts = {}) {
-    super()
-    this.filePath = filePath
-    this.fd = fs.openSync(filePath, "a+")
-    this.namespace = opts.namespace
-  }
+	constructor(filePath: string, opts: AppendOnlyKeyvOpts = {}) {
+		super();
+		this.filePath = filePath;
+		this.fd = fs.openSync(filePath, "a+");
+		this.namespace = opts.namespace;
+	}
 
-  get opts() {
-    return {
-      filePath: this.filePath,
-      fd: this.fd,
-      currentOffset: this.offset,
-    }
-  }
+	get opts() {
+		return {
+			filePath: this.filePath,
+			fd: this.fd,
+			currentOffset: this.offset,
+		};
+	}
 
-  async get<Value = unknown>(key: string): Promise<StoredData<Value> | undefined> {
-    const record = this.index[key];
-    if (!record) return undefined;
-    const kl = this.messagePrefix(key).length;
-    const bytesRead = record.length - kl;
-    const buffer = Buffer.alloc(bytesRead);
+	async get<Value = unknown>(
+		key: string,
+	): Promise<StoredData<Value> | undefined> {
+		const record = this.index[key];
+		if (!record) return undefined;
+		const kl = this.messagePrefix(key).length;
+		const bytesRead = record.length - kl;
+		const buffer = Buffer.alloc(bytesRead);
 
-    return new Promise<StoredData<Value> | undefined>((resolve, reject) => {
-      fs.read(this.fd, buffer, 0, bytesRead, record.offset + kl, (err) => {
-        if (err) return reject(err);
+		return new Promise<StoredData<Value> | undefined>((resolve, reject) => {
+			fs.read(this.fd, buffer, 0, bytesRead, record.offset + kl, (err) => {
+				if (err) return reject(err);
 
-        const parsed: StoredData<Value> = JSON.parse(buffer.toString().trim());
-        resolve((parsed as any).value.payload);
-      });
-    });
-  }
+				const parsed = JSON.parse(buffer.toString().trim());
+				resolve(parsed.value.payload);
+			});
+		});
+	}
 
-  async set(key: string, value: string) {
-    const message = this.buildMessage(value)
-    const serializedMessage = this.serializeMessage(key, message)
-    const bytesWritten = fs.writevSync(this.fd, [Buffer.from(serializedMessage)])
-    this.index[key] = { length: bytesWritten, offset: this.offset };
-    this.offset += bytesWritten
-    return 'OK' as const
-  }
+	async set(key: string, value: string) {
+		const message = this.buildMessage(value);
+		const serializedMessage = this.serializeMessage(key, message);
+		const bytesWritten = fs.writevSync(this.fd, [
+			Buffer.from(serializedMessage),
+		]);
+		this.index[key] = { length: bytesWritten, offset: this.offset };
+		this.offset += bytesWritten;
+		return "OK" as const;
+	}
 
-  async delete(key: string) {
-    const message = this.buildMessage(undefined, { deleted: true })
-    const serializedMessage = this.serializeMessage(key, message)
-    this.index[key] = undefined
-    return new Promise<boolean>((resolve,) => {
-      fs.writev(this.fd, [Buffer.from(serializedMessage)], () => {
-        resolve(true)
-      });
+	async delete(key: string) {
+		const message = this.buildMessage(undefined, { deleted: true });
+		const serializedMessage = this.serializeMessage(key, message);
+		this.index[key] = undefined;
+		return new Promise<boolean>((resolve) => {
+			fs.writev(this.fd, [Buffer.from(serializedMessage)], () => {
+				resolve(true);
+			});
+		});
+	}
 
-    })
-  }
+	clear(): Promise<void> {
+		this.index = {};
+		return Promise.resolve();
+	}
 
-  clear(): Promise<void> {
-    this.index = {}
-    return Promise.resolve()
-  }
+	has(key: string): Promise<boolean> {
+		const record = this.index[key];
+		return Promise.resolve(Boolean(record));
+	}
 
-  has(key: string): Promise<boolean> {
-    const record = this.index[key];
-    return Promise.resolve(Boolean(record))
-  }
+	disconnect(): Promise<void> {
+		return new Promise((resolve, reject) => {
+			fs.close(this.fd, (err) => {
+				if (err) {
+					return reject(err);
+				}
+				return resolve();
+			});
+		});
+	}
 
-  disconnect(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      fs.close(this.fd, (err) => {
-        if (err) {
-          return reject(err)
-        }
-        return resolve()
-      })
-    })
-  }
+	private buildMessage(
+		value: string | undefined,
+		{ deleted }: { deleted?: boolean } = {},
+	): StoredData<Message> {
+		return {
+			value: {
+				payload: value,
+				timestamp: Date.now(),
+				deleted,
+			},
+			expires: undefined,
+		} satisfies StoredData<Message>;
+	}
 
-  private buildMessage(value: string | undefined, { deleted }: { deleted?: boolean } = {}): StoredData<Message> {
-    return {
-      value: {
-        payload: value,
-        timestamp: Date.now(),
-        deleted,
-      },
-      expires: undefined,
-    } satisfies StoredData<Message>
-  }
+	private serializeMessage(key: string, msg: StoredData<Message>) {
+		return `${this.messagePrefix(key)}${JSON.stringify(msg)}`;
+	}
 
-  private serializeMessage(key: string, msg: StoredData<Message>) {
-    return `${this.messagePrefix(key)}${JSON.stringify(msg)}`
-  }
-
-  private messagePrefix(key: string) {
-    if (this.namespace) return `${this.namespace}:${key}: `
-    return key + ": "
-  }
+	private messagePrefix(key: string) {
+		if (this.namespace) return `${this.namespace}:${key}: `;
+		return `${key}: `;
+	}
 }
--- tests/db.spec.ts
+++ tests/db.spec.ts
@@ -1,130 +1,136 @@
-import { afterEach, describe, expect, it } from 'vitest'
-import fs from 'node:fs'
-import path from 'node:path'
-import { AppendOnlyKeyv } from '../src/db/client'
+import fs from "node:fs";
+import path from "node:path";
+import { afterEach, describe, expect, it } from "vitest";
+import { AppendOnlyKeyv } from "../src/db/client";
 
 describe("AppendOnlyClient", () => {
-  const filename = Math.random().toString(36).substring(7) + ".txt"
+	const filename = `${Math.random().toString(36).substring(7)}.txt`;
 
-  afterEach(() => {
-    fs.unlinkSync(filename)
-  })
+	afterEach(() => {
+		fs.unlinkSync(filename);
+	});
 
-  describe("set method", () => {
-    it("returns 'OK' upon a successful set", async () => {
-      const client = new AppendOnlyKeyv(filename)
+	describe("set method", () => {
+		it("returns 'OK' upon a successful set", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      const res = await client.set("key", "value")
-      expect(res).toBe("OK")
-    })
+			const res = await client.set("key", "value");
+			expect(res).toBe("OK");
+		});
 
-    it("writes the key-value pair to the file", async () => {
-      const client = new AppendOnlyKeyv(filename)
+		it("writes the key-value pair to the file", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("key", "value")
+			await client.set("key", "value");
 
-      const content = fs.readFileSync(path.join(filename), "utf8").split('\n').filter(Boolean).pop()
-      expect(content).contain("value")
-      expect(content).contain("key")
-      expect(content).contain("timestamp")
-    })
-  })
+			const content = fs
+				.readFileSync(path.join(filename), "utf8")
+				.split("\n")
+				.filter(Boolean)
+				.pop();
+			expect(content).contain("value");
+			expect(content).contain("key");
+			expect(content).contain("timestamp");
+		});
+	});
 
-  describe("get method", () => {
-    it("returns undefined if the key doesn't exist", async () => {
-      const client = new AppendOnlyKeyv(filename)
+	describe("get method", () => {
+		it("returns undefined if the key doesn't exist", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      expect(await client.get("key")).toBeUndefined()
-    })
+			expect(await client.get("key")).toBeUndefined();
+		});
 
-    it("works with namespaced keys", async () => {
-      const client = new AppendOnlyKeyv(filename, { namespace: "ns" })
+		it("works with namespaced keys", async () => {
+			const client = new AppendOnlyKeyv(filename, { namespace: "ns" });
 
-      client.set("key", "value")
+			client.set("key", "value");
 
-      expect(await client.get("key")).toBe("value")
-    })
+			expect(await client.get("key")).toBe("value");
+		});
 
-    it("returns the value for the given key", async () => {
-      const client = new AppendOnlyKeyv(filename)
+		it("returns the value for the given key", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("key", "value - 1")
+			await client.set("key", "value - 1");
 
-      expect(await client.get("key")).toBe("value - 1")
-    })
+			expect(await client.get("key")).toBe("value - 1");
+		});
 
-    it("returns the right key even after multiple different sets", async () => {
-      const client = new AppendOnlyKeyv(filename)
+		it("returns the right key even after multiple different sets", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      client.set("key", "value")
-      client.set("key 1", "value - 1")
-      client.set("key 2", "value - 2")
-      client.set("key 3", "value - 3")
+			client.set("key", "value");
+			client.set("key 1", "value - 1");
+			client.set("key 2", "value - 2");
+			client.set("key 3", "value - 3");
 
-      expect(await client.get("key 2")).toBe("value - 2")
-      expect(await client.get("key")).toBe("value")
-    })
-  })
+			expect(await client.get("key 2")).toBe("value - 2");
+			expect(await client.get("key")).toBe("value");
+		});
+	});
 
-  describe("delete method", () => {
-    it("returns true", async () => {
-      const client = new AppendOnlyKeyv(filename)
+	describe("delete method", () => {
+		it("returns true", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("a", "b")
-      const res = await client.delete("a")
-      expect(res).toBe(true)
-    })
+			await client.set("a", "b");
+			const res = await client.delete("a");
+			expect(res).toBe(true);
+		});
 
-    it("adds a tombstone to the file", async () => {
-      const client = new AppendOnlyKeyv(filename)
+		it("adds a tombstone to the file", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("a", "b")
-      await client.delete("a")
+			await client.set("a", "b");
+			await client.delete("a");
 
-      const content = fs.readFileSync(filename)
-      expect(content.toString()).toContain("deleted\":true")
-    })
+			const content = fs.readFileSync(filename);
+			expect(content.toString()).toContain('deleted":true');
+		});
 
-    it("removes the key from the index", async () => {
-      const client = new AppendOnlyKeyv(filename)
+		it("removes the key from the index", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("a", "b")
-      await client.delete("a")
+			await client.set("a", "b");
+			await client.delete("a");
 
-      expect(await client.get("a")).toBeUndefined()
-    })
-  })
+			expect(await client.get("a")).toBeUndefined();
+		});
+	});
 
-  describe("opts getter", () => {
-    it("returns an object", () => {
-      const client = new AppendOnlyKeyv(filename)
+	describe("opts getter", () => {
+		it("returns an object", () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      expect(client.opts.filePath).toBe(filename)
-      expect(client.opts).toEqual(expect.objectContaining({
-        currentOffset: expect.any(Number),
-        fd: expect.any(Number)
-      }))
-    })
-  })
+			expect(client.opts.filePath).toBe(filename);
+			expect(client.opts).toEqual(
+				expect.objectContaining({
+					currentOffset: expect.any(Number),
+					fd: expect.any(Number),
+				}),
+			);
+		});
+	});
 
-  describe("clear method", () => {
-    it("clears the index", async () => {
-      const client = new AppendOnlyKeyv(filename)
+	describe("clear method", () => {
+		it("clears the index", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("a", "b")
-      await client.clear()
+			await client.set("a", "b");
+			await client.clear();
 
-      expect(await client.get("a")).toBeUndefined()
-    })
-  })
+			expect(await client.get("a")).toBeUndefined();
+		});
+	});
 
-  describe("has method", () => {
-    it("returns true if the key exists", async () => {
-      const client = new AppendOnlyKeyv(filename)
+	describe("has method", () => {
+		it("returns true if the key exists", async () => {
+			const client = new AppendOnlyKeyv(filename);
 
-      await client.set("a", "b")
+			await client.set("a", "b");
 
-      expect(await client.has("a")).toBe(true)
-    })
-  })
-})
+			expect(await client.has("a")).toBe(true);
+		});
+	});
+});
